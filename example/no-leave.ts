8e5c07e1130e54d0571e96c8347d64e3bf1fa387
Eric Rabil <ericjrabil@gmail.com>
2020-09-01 17:40:25 -0400

    Added reference no-leave bot

---
+++ example/no-leave.ts
@@ -0,0 +1,17 @@
+import { Client } from "../src";
+import { ChatParticipantsChanged } from "../src/client/client-events";
+
+const client = new Client();
+
+const groupID = 'DD0712E5-D0CD-4CA0-B2F5-7A3700BBA5CA';
+const handleID = '+16475469749';
+
+client.on(ChatParticipantsChanged, chat => {
+    if (chat.groupID !== groupID) return;
+    if (!chat.participantIDs.includes(handleID)) {
+        chat.addParticipants([handleID]);
+        console.log('shimmy shimmy ya')
+    }
+})
+
+client.connect();
